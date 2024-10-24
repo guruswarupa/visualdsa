a48c8678e37a5f7d373fe02839fe3f1f29b80294
Naresh Karthigeyan <130460278+nareshkarthigeyan@users.noreply.github.com>
2024-10-24 03:55:51 +0000

    selection sort

--- public/sorting-algorithms/selectionsort/code.cs
+++ public/sorting-algorithms/selectionsort/code.cs
@@ -1,12 +1,13 @@
-// Java program for implementation of Selection Sort
-import java.util.Arrays;
+// C# program for implementation
+// of Selection Sort
+using System;
 
 class GfG {
 
     static void selectionSort(int[] arr){
-        int n = arr.length;
+        int n = arr.Length;
         for (int i = 0; i < n - 1; i++) {
-          
+
             // Assume the current position holds
             // the minimum element
             int min_idx = i;
@@ -15,37 +16,37 @@ class GfG {
             // to find the actual minimum
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[min_idx]) {
-                  
+
                     // Update min_idx if a smaller element
                     // is found
                     min_idx = j;
                 }
             }
 
-            // Move minimum element to its
-            // correct position
-            int temp = arr[i];
-            arr[i] = arr[min_idx];
-            arr[min_idx] = temp;           
+           // Move minimum element to its
+           // correct position
+           int temp = arr[i];
+           arr[i] = arr[min_idx];
+           arr[min_idx] = temp;         
         }
     }
 
     static void printArray(int[] arr){
-        for (int val : arr) {
-            System.out.print(val + " ");
+        foreach(int val in arr){
+            Console.Write(val + " ");
         }
-        System.out.println();
+        Console.WriteLine();
     }
-  
-    public static void main(String[] args){
+
+    public static void Main(){
         int[] arr = { 64, 25, 12, 22, 11 };
 
-        System.out.print("Original array: ");
+        Console.Write("Original array: ");
         printArray(arr);
 
         selectionSort(arr);
 
-        System.out.print("Sorted array: ");
+        Console.Write("Sorted array: ");
         printArray(arr);
     }
 }
